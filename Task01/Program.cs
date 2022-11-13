ee31158b6cd21a0b1867f4b7b2017247dd5a39d3
Stelsor95 <silaa7465@gmail.com>
2022-11-13 17:00:00 +0300

    Дополнил папку Task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -7,3 +7,14 @@
 // a = 2, b = 10 -> нет
 // a = 9, b = -3 -> да
 // a = -3 b = 9 -> нет
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b * b)
+{
+    Console.WriteLine($"Число {a} является квадратом числа {b}");
+}
+else Console.WriteLine($"Число {a} не является квадратом числа {b}");
